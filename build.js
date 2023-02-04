8ffee2adc7806fffffc76a7d9ac517dcc7febbe4
Phonedolly <c1john0803@gmail.com>
2023-02-04 22:06:20 +0000

    support sitemap

--- build.js
+++ build.js
@@ -3,12 +3,13 @@ import path from 'path';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import shell from 'shelljs';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
+import axios from 'axios';
 
 const argv = yargs(hideBin(process.argv)).argv;
 const projectName = argv.projectName;
 const homeDir = argv.homeDir;
-const config = dotenv.config({ path: path.join(homeDir, `${projectName}.env`) })
+const buildConfig = dotenv.config({ path: path.join(homeDir, `${projectName}.env`) })
 
 const ecosystem = `module.exports = ${JSON.stringify({
   apps: [
@@ -16,7 +17,7 @@ const ecosystem = `module.exports = ${JSON.stringify({
       name: projectName,
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
-      env_production: config.parsed,
+      env_production: buildConfig.parsed,
     }
   ]
 })}`
@@ -34,19 +35,20 @@ try {
 try {
   shell.exec(`git clone https://github.com/Phonedolly/next-turnip ${projectName}_${argv.buildStartTime}`);
   shell.cd(`${projectName}_${argv.buildStartTime}`);
-  shell.exec(`npm ci`).toString()
-  shell.exec(`REMOTE_PATTERNS_URLS=${config.parsed.REMOTE_PATTERNS_URLS} npm run build`).toString()
-  shell.exec(`pm2 delete ${projectName}`).toString()
+  shell.exec(`npm ci`);
+  shell.exec(`REMOTE_PATTERNS_URLS=${buildConfig.parsed.REMOTE_PATTERNS_URLS} npm run build`);
+  axios.get(`https://www.google.com/ping?sitemap=${buildConfig.SITE_URL}/sitemap.xml`);
+  shell.exec(`pm2 delete ${projectName}`);
   // shell.cp(path.join(homeDir, `${projectName}.ecosystem.config.js`), `./ecosystem.config.js`)
-  fs.writeFileSync(path.join(homeDir, `ecosystem.config.js.tmp`), ecosystem)
-  shell.mv(path.join(homeDir, `ecosystem.config.js.tmp`), './ecosystem.config.js')
-  shell.exec(`pm2 start ecosystem.config.js --env production`).toString()
-  process.send({ isBuildSuccess: true })
+  fs.writeFileSync(path.join(homeDir, `ecosystem.config.js.tmp`), ecosystem);
+  shell.mv(path.join(homeDir, `ecosystem.config.js.tmp`), './ecosystem.config.js');
+  shell.exec(`pm2 start ecosystem.config.js --env production`);
+  process.send({ isBuildSuccess: true });
   process.exit(0);
 } catch (err) {
   console.error('build failed!');
   console.error(err);
-  process.send({ isBuildSuccess: false })
+  process.send({ isBuildSuccess: false });
   process.exit(1);
 }
 
